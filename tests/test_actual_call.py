b211864e53937cc6cd5bf70b872db422f121ba32
Tomas Correa <tomas.correa@gmail.com>
2023-10-14 21:45:57 +0000

    removing files

--- tests/test_actual_call.py
+++
@@ -1,9 +0,0 @@
-# def test_conductor_task_case(celery_app, celery_worker):
-
-
-#     @celery_app.task(base=ConductorTask, name="legal_entity_enrich_person")
-#     def mul():
-#         return {"total": 2}
-
-#     celery_worker.reload()
-#     assert mul.apply().result == {"total": 8}

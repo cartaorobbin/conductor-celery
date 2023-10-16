61e1c0f7a66dffcf2f756cd17c0137c948104bb7
Tomas Correa <tomas.correa@gmail.com>
2023-10-16 15:11:14 +0000

    refactor, change httppretty for responses

--- tests/test_conductor_task.py
+++ tests/test_conductor_task.py
@@ -1,21 +1,21 @@
 import json
 import socket
 
-import httpretty
+# import httpretty
 
 from conductor_celery.tasks import ConductorTask
 
-httpretty.enable(allow_net_connect=False, verbose=True)
+# httpretty.enable(allow_net_connect=False, verbose=True)
 
 
-def test_conductor_task_case_1(celery_app, celery_worker, task_poll_response):
-    httpretty.register_uri(
-        httpretty.GET,
-        "https://localhost:8080/api/tasks/poll/celery_test_task?workerid=localhost",
+def test_conductor_task_case_1(celery_app, celery_worker, task_poll_response, responses):
+    worker_id = socket.gethostname()
+    responses.get(
+        f"https://localhost:8080/api/tasks/poll/celery_test_task?workerid={worker_id}",
         body=task_poll_response({"x": 2, "y": 4}),
     )
 
-    httpretty.register_uri(httpretty.POST, "https://localhost:8080/api/tasks", body="1233444")
+    responses.post("https://localhost:8080/api/tasks", body="1233444")
 
     @celery_app.task(base=ConductorTask, name="celery_test_task")
     def mul(x, y):
@@ -24,27 +24,27 @@ def test_conductor_task_case_1(celery_app, celery_worker, task_poll_response):
     celery_worker.reload()
     assert mul.apply([2, 2]).result == {"total": 8}
 
-    assert httpretty.latest_requests()[-1].url == "https://localhost:8080/api/tasks"
-    body = json.loads(httpretty.latest_requests()[-1].body)
+    assert responses.calls[-1].request.url == "https://localhost:8080/api/tasks"
+    body = json.loads(responses.calls[-1].request.body)
 
     assert body["outputData"] == {"total": 8}
     assert body["status"] == "COMPLETED"
 
     assert body["workflowInstanceId"] == "18bfeabf-3a1c-11ee-868b-06fd3bd0ae8b"
     assert body["taskId"] == "18c0fc30-3a1c-11ee-868b-06fd3bd0ae8b"
-    assert body["workerId"] == "localhost"
+    assert body["workerId"] == worker_id
 
     assert list(body.keys()) == ["workflowInstanceId", "taskId", "workerId", "status", "outputData"]
 
 
-def test_conductor_task_case_2(celery_app, celery_worker, task_poll_response):
-    httpretty.register_uri(
-        httpretty.GET,
-        "https://localhost:8080/api/tasks/poll/celery_test_task?workerid=localhost",
+def test_conductor_task_case_2(celery_app, celery_worker, task_poll_response, responses):
+    worker_id = socket.gethostname()
+    responses.get(
+        f"https://localhost:8080/api/tasks/poll/celery_test_task?workerid={worker_id}",
         body=task_poll_response({"x": 3, "y": 4}),
     )
 
-    httpretty.register_uri(httpretty.POST, "https://localhost:8080/api/tasks")
+    responses.post("https://localhost:8080/api/tasks", body="1233444")
 
     @celery_app.task(base=ConductorTask, name="celery_test_task")
     def mul(x, y):
@@ -53,27 +53,27 @@ def test_conductor_task_case_2(celery_app, celery_worker, task_poll_response):
     celery_worker.reload()
     assert mul.apply([2, 2]).result == {"total": 12}
 
-    assert httpretty.latest_requests()[-1].url == "https://localhost:8080/api/tasks"
-    body = json.loads(httpretty.latest_requests()[-1].body)
+    assert responses.calls[-1].request.url == "https://localhost:8080/api/tasks"
+    body = json.loads(responses.calls[-1].request.body)
 
     assert body["outputData"] == {"total": 12}
     assert body["status"] == "COMPLETED"
 
     assert body["workflowInstanceId"] == "18bfeabf-3a1c-11ee-868b-06fd3bd0ae8b"
     assert body["taskId"] == "18c0fc30-3a1c-11ee-868b-06fd3bd0ae8b"
-    assert body["workerId"] == "localhost"
+    assert body["workerId"] == worker_id
 
     assert list(body.keys()) == ["workflowInstanceId", "taskId", "workerId", "status", "outputData"]
 
 
-def test_conductor_task_case_3(celery_app, celery_worker, task_poll_response):
-    httpretty.register_uri(
-        httpretty.GET,
-        "https://localhost:8080/api/tasks/poll/celery_test_task?workerid=localhost",
+def test_conductor_task_case_3(celery_app, celery_worker, task_poll_response, responses):
+    worker_id = socket.gethostname()
+    responses.get(
+        f"https://localhost:8080/api/tasks/poll/celery_test_task?workerid={worker_id}",
         body=task_poll_response({"x": 3, "y": 4}),
     )
 
-    httpretty.register_uri(httpretty.POST, "https://localhost:8080/api/tasks")
+    responses.post("https://localhost:8080/api/tasks")
 
     @celery_app.task(base=ConductorTask, name="celery_test_task")
     def mul(x, y):
@@ -82,29 +82,29 @@ def test_conductor_task_case_3(celery_app, celery_worker, task_poll_response):
     celery_worker.reload()
     assert mul.apply().result == {"total": 12}
 
-    assert httpretty.latest_requests()[-1].url == "https://localhost:8080/api/tasks"
-    body = json.loads(httpretty.latest_requests()[-1].body)
+    assert responses.calls[-1].request.url == "https://localhost:8080/api/tasks"
+    body = json.loads(responses.calls[-1].request.body)
 
     assert body["outputData"] == {"total": 12}
     assert body["status"] == "COMPLETED"
 
     assert body["workflowInstanceId"] == "18bfeabf-3a1c-11ee-868b-06fd3bd0ae8b"
     assert body["taskId"] == "18c0fc30-3a1c-11ee-868b-06fd3bd0ae8b"
-    assert body["workerId"] == "localhost"
+    assert body["workerId"] == worker_id
 
     assert list(body.keys()) == ["workflowInstanceId", "taskId", "workerId", "status", "outputData"]
 
 
-def test_conductor_task_task_name(celery_app, celery_worker, task_poll_response):
+def test_conductor_task_task_name(celery_app, celery_worker, task_poll_response, responses):
+    worker_id = socket.gethostname()
     my_task_name = "task_name"
 
-    httpretty.register_uri(
-        httpretty.GET,
-        f"https://localhost:8080/api/tasks/poll/{my_task_name}?workerid=localhost",
+    responses.get(
+        f"https://localhost:8080/api/tasks/poll/{my_task_name}?workerid={worker_id}",
         body=task_poll_response({"x": 3, "y": 4}),
     )
 
-    httpretty.register_uri(httpretty.POST, "https://localhost:8080/api/tasks")
+    responses.post("https://localhost:8080/api/tasks")
 
     @celery_app.task(base=ConductorTask, name=my_task_name)
     def mul(x, y):
@@ -113,29 +113,29 @@ def test_conductor_task_task_name(celery_app, celery_worker, task_poll_response)
     celery_worker.reload()
     assert mul.apply().result == {"total": 12}
 
-    assert httpretty.latest_requests()[-1].url == "https://localhost:8080/api/tasks"
-    body = json.loads(httpretty.latest_requests()[-1].body)
+    assert responses.calls[-1].request.url == "https://localhost:8080/api/tasks"
+    body = json.loads(responses.calls[-1].request.body)
 
     assert body["outputData"] == {"total": 12}
     assert body["status"] == "COMPLETED"
 
     assert body["workflowInstanceId"] == "18bfeabf-3a1c-11ee-868b-06fd3bd0ae8b"
     assert body["taskId"] == "18c0fc30-3a1c-11ee-868b-06fd3bd0ae8b"
-    assert body["workerId"] == "localhost"
+    assert body["workerId"] == worker_id
 
     assert list(body.keys()) == ["workflowInstanceId", "taskId", "workerId", "status", "outputData"]
 
 
-def test_conductor_task_bind_true(celery_app, celery_worker, task_poll_response):
+def test_conductor_task_bind_true(celery_app, celery_worker, task_poll_response, responses):
+    worker_id = socket.gethostname()
     my_task_name = "task_name"
 
-    httpretty.register_uri(
-        httpretty.GET,
-        f"https://localhost:8080/api/tasks/poll/{my_task_name}?workerid=localhost",
+    responses.get(
+        f"https://localhost:8080/api/tasks/poll/{my_task_name}?workerid={worker_id}",
         body=task_poll_response({"x": 3, "y": 4}),
     )
 
-    httpretty.register_uri(httpretty.POST, "https://localhost:8080/api/tasks")
+    responses.post("https://localhost:8080/api/tasks")
 
     @celery_app.task(base=ConductorTask, name=my_task_name, bind=True)
     def mul(self, x, y):
@@ -144,30 +144,29 @@ def test_conductor_task_bind_true(celery_app, celery_worker, task_poll_response)
     celery_worker.reload()
     assert mul.apply().result == {"total": 12}
 
-    assert httpretty.latest_requests()[-1].url == "https://localhost:8080/api/tasks"
-    body = json.loads(httpretty.latest_requests()[-1].body)
+    assert responses.calls[-1].request.url == "https://localhost:8080/api/tasks"
+    body = json.loads(responses.calls[-1].request.body)
 
     assert body["outputData"] == {"total": 12}
     assert body["status"] == "COMPLETED"
 
     assert body["workflowInstanceId"] == "18bfeabf-3a1c-11ee-868b-06fd3bd0ae8b"
     assert body["taskId"] == "18c0fc30-3a1c-11ee-868b-06fd3bd0ae8b"
-    assert body["workerId"] == "localhost"
+    assert body["workerId"] == worker_id
 
     assert list(body.keys()) == ["workflowInstanceId", "taskId", "workerId", "status", "outputData"]
 
 
-def test_conductor_task_other_signatures(celery_app, celery_worker, task_poll_response):
+def test_conductor_task_other_signatures(celery_app, celery_worker, task_poll_response, responses):
     worker_id = socket.gethostname()
     my_task_name = "task_name"
 
-    httpretty.register_uri(
-        httpretty.GET,
+    responses.get(
         f"https://localhost:8080/api/tasks/poll/{my_task_name}?workerid={worker_id}",
         body=task_poll_response({"company_id": "3"}),
     )
 
-    httpretty.register_uri(httpretty.POST, "https://localhost:8080/api/tasks")
+    responses.post("https://localhost:8080/api/tasks")
 
     @celery_app.task(base=ConductorTask, name=my_task_name, bind=True)
     def mul(self, company_id):
@@ -176,31 +175,30 @@ def test_conductor_task_other_signatures(celery_app, celery_worker, task_poll_re
     celery_worker.reload()
     assert mul.apply().result == {"company": "3"}
 
-    assert httpretty.latest_requests()[-1].url == "https://localhost:8080/api/tasks"
-    body = json.loads(httpretty.latest_requests()[-1].body)
+    assert len(responses.calls) == 2
+    assert responses.calls[-1].request.url == "https://localhost:8080/api/tasks"
+    body = json.loads(responses.calls[-1].request.body)
 
     assert body["outputData"] == {"company": "3"}
     assert body["status"] == "COMPLETED"
 
     assert body["workflowInstanceId"] == "18bfeabf-3a1c-11ee-868b-06fd3bd0ae8b"
     assert body["taskId"] == "18c0fc30-3a1c-11ee-868b-06fd3bd0ae8b"
-    assert body["workerId"] == "localhost"
+    assert body["workerId"] == worker_id
 
     assert list(body.keys()) == ["workflowInstanceId", "taskId", "workerId", "status", "outputData"]
 
 
-def test_conductor_task_delay(celery_app, celery_worker, task_poll_response):
-    httpretty.reset()
+def test_conductor_task_delay(celery_app, celery_worker, task_poll_response, responses):
     worker_id = socket.gethostname()
     my_task_name = "task_name"
 
-    httpretty.register_uri(
-        httpretty.GET,
+    responses.get(
         f"https://localhost:8080/api/tasks/poll/{my_task_name}?workerid={worker_id}",
         body=task_poll_response({"company_id": "3"}),
     )
 
-    httpretty.register_uri(httpretty.POST, "https://localhost:8080/api/tasks")
+    responses.post("https://localhost:8080/api/tasks")
 
     @celery_app.task(base=ConductorTask, name=my_task_name, bind=True)
     def mul(self, company_id):
@@ -208,78 +206,78 @@ def test_conductor_task_delay(celery_app, celery_worker, task_poll_response):
 
     celery_worker.reload()
     assert mul.delay(2).get() == {"company": "3"}
-    # import pytest; pytest.set_trace()
-    # # assert len(httpretty.latest_requests()) == 2
-    assert httpretty.latest_requests()[-1].url == "https://localhost:8080/api/tasks"
-    body = json.loads(httpretty.latest_requests()[-1].body)
+
+    assert len(responses.calls) == 2
+    assert responses.calls[-1].request.url == "https://localhost:8080/api/tasks"
+    body = json.loads(responses.calls[-1].request.body)
 
     assert body["outputData"] == {"company": "3"}
     assert body["status"] == "COMPLETED"
 
     assert body["workflowInstanceId"] == "18bfeabf-3a1c-11ee-868b-06fd3bd0ae8b"
     assert body["taskId"] == "18c0fc30-3a1c-11ee-868b-06fd3bd0ae8b"
-    assert body["workerId"] == "localhost"
+    assert body["workerId"] == worker_id
 
     assert list(body.keys()) == ["workflowInstanceId", "taskId", "workerId", "status", "outputData"]
 
 
-def test_conductor_task_error_with_auto_retry(celery_app, celery_worker, task_poll_response):
-    httpretty.reset()
-    httpretty.register_uri(
-        httpretty.GET,
-        "https://localhost:8080/api/tasks/poll/celery_test_task?workerid=localhost",
+def test_conductor_task_error_with_auto_retry(celery_app, celery_worker, task_poll_response, responses):
+    worker_id = socket.gethostname()
+    my_task_name = "celery_test_task"
+    responses.get(
+        f"https://localhost:8080/api/tasks/poll/{my_task_name}?workerid={worker_id}",
         body=task_poll_response({"x": 2, "y": 0}),
     )
 
-    httpretty.register_uri(httpretty.POST, "https://localhost:8080/api/tasks", body="1233444")
+    responses.post("https://localhost:8080/api/tasks")
 
-    @celery_app.task(base=ConductorTask, name="celery_test_task", autoretry_for=(ZeroDivisionError,))
+    @celery_app.task(base=ConductorTask, name=my_task_name, autoretry_for=(ZeroDivisionError,))
     def div(x, y):
         return {"total": x / y}
 
     celery_worker.reload()
     assert div.apply([2, 0]).result.__class__ == ZeroDivisionError
 
-    assert len(httpretty.latest_requests()) == 3
-    assert httpretty.latest_requests()[-1].url == "https://localhost:8080/api/tasks"
-    body = json.loads(httpretty.latest_requests()[-1].body)
+    assert len(responses.calls) == 2
+    assert responses.calls[-1].request.url == "https://localhost:8080/api/tasks"
+    body = json.loads(responses.calls[-1].request.body)
 
     assert body["outputData"] == {"error": "division by zero"}
     assert body["status"] == "FAILED"
 
     assert body["workflowInstanceId"] == "18bfeabf-3a1c-11ee-868b-06fd3bd0ae8b"
     assert body["taskId"] == "18c0fc30-3a1c-11ee-868b-06fd3bd0ae8b"
-    assert body["workerId"] == "localhost"
+    assert body["workerId"] == worker_id
 
     assert list(body.keys()) == ["workflowInstanceId", "taskId", "workerId", "status", "outputData"]
 
 
-def test_conductor_task_error_without_auto_retry(celery_app, celery_worker, task_poll_response):
-    httpretty.reset()
-    httpretty.register_uri(
-        httpretty.GET,
-        "https://localhost:8080/api/tasks/poll/celery_test_task?workerid=localhost",
+def test_conductor_task_error_without_auto_retry(celery_app, celery_worker, task_poll_response, responses):
+    worker_id = socket.gethostname()
+    my_task_name = "celery_test_task"
+    responses.get(
+        f"https://localhost:8080/api/tasks/poll/{my_task_name}?workerid={worker_id}",
         body=task_poll_response({"x": 2, "y": 0}),
     )
 
-    httpretty.register_uri(httpretty.POST, "https://localhost:8080/api/tasks", body="1233444")
+    responses.post("https://localhost:8080/api/tasks")
 
-    @celery_app.task(base=ConductorTask, name="celery_test_task")
+    @celery_app.task(base=ConductorTask, name=my_task_name)
     def div(x, y):
         return {"total": x / y}
 
     celery_worker.reload()
     assert div.apply([2, 0]).result.__class__ == ZeroDivisionError
 
-    assert len(httpretty.latest_requests()) == 3
-    assert httpretty.latest_requests()[-1].url == "https://localhost:8080/api/tasks"
-    body = json.loads(httpretty.latest_requests()[-1].body)
+    assert len(responses.calls) == 2
+    assert responses.calls[-1].request.url == "https://localhost:8080/api/tasks"
+    body = json.loads(responses.calls[-1].request.body)
 
     assert body["outputData"] == {"error": "division by zero"}
     assert body["status"] == "FAILED"
 
     assert body["workflowInstanceId"] == "18bfeabf-3a1c-11ee-868b-06fd3bd0ae8b"
     assert body["taskId"] == "18c0fc30-3a1c-11ee-868b-06fd3bd0ae8b"
-    assert body["workerId"] == "localhost"
+    assert body["workerId"] == worker_id
 
     assert list(body.keys()) == ["workflowInstanceId", "taskId", "workerId", "status", "outputData"]
--- tests/test_update_task.py
+++ tests/test_update_task.py
@@ -1,22 +1,19 @@
 import json
 
-import httpretty
 
 from conductor_celery.tasks import update_task
 from conductor_celery.utils import configure_runner
+import socket
 
-httpretty.enable(allow_net_connect=False, verbose=True)
 
-
-def test_update_task(celery_app, celery_worker, task_poll_response):
-    httpretty.reset()
-    httpretty.register_uri(
-        httpretty.GET,
-        "https://localhost:8080/api/tasks/poll/celery_test_task?workerid=localhost",
+def test_update_task(celery_app, celery_worker, task_poll_response, responses):
+    worker_id = socket.gethostname()
+    responses.get(
+        f"https://localhost:8080/api/tasks/poll/celery_test_task?workerid={worker_id}",
         body=task_poll_response({"x": 3, "y": 4}),
     )
 
-    httpretty.register_uri(httpretty.POST, "https://localhost:8080/api/tasks")
+    responses.post("https://localhost:8080/api/tasks", body="1233444")
 
     name = "celery_test_task"
 
@@ -34,14 +31,14 @@ def test_update_task(celery_app, celery_worker, task_poll_response):
 
     update_task.apply(kwargs=payload).get()
 
-    assert httpretty.latest_requests()[-1].url == "https://localhost:8080/api/tasks"
-    body = json.loads(httpretty.latest_requests()[-1].body)
+    assert responses.calls[-1].request.url == "https://localhost:8080/api/tasks"
+    body = json.loads(responses.calls[-1].request.body)
 
     assert body["outputData"] == {"total": 8}
     assert body["status"] == "COMPLETED"
 
     assert body["workflowInstanceId"] == "18bfeabf-3a1c-11ee-868b-06fd3bd0ae8b"
     assert body["taskId"] == "18c0fc30-3a1c-11ee-868b-06fd3bd0ae8b"
-    assert body["workerId"] == "localhost"
+    assert body["workerId"] == worker_id
 
     assert list(body.keys()) == ["workflowInstanceId", "taskId", "workerId", "status", "outputData"]
